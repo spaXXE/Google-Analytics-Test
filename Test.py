a35fc2deff8775759870b4fd169226a84e0826f7
spaXXE <114006762+spaXXE@users.noreply.github.com>
2024-05-11 18:18:29 +0300

    shifted position of GA 4 tage

--- Test.py
+++ Test.py
@@ -6,14 +6,6 @@ from pathlib import Path
 
 st.set_page_config(page_title='GOOGLE ANALYTICS TEST', page_icon='🏢', layout="centered", initial_sidebar_state="collapsed")
 
-hide_streamlit_style = """ 
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-
 google_analytics = """ 
             <!-- Google tag (gtag.js) -->
             <script async src="https://www.googletagmanager.com/gtag/js?id=G-TSM5N1J350"></script>
@@ -26,5 +18,15 @@ google_analytics = """
             </script>
             """
 st.markdown(google_analytics, unsafe_allow_html=True)
+
+hide_streamlit_style = """ 
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
 
 st.write("Hello World")
