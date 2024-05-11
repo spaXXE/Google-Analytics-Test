9818de1a97cc9aaed52639bdf4c2cd82626577e7
spaXXE <114006762+spaXXE@users.noreply.github.com>
2024-05-11 18:20:41 +0300

    added GA 4 tag

--- Test.py
+++ Test.py
@@ -1,3 +1,15 @@
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-TSM5N1J350"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-TSM5N1J350');
+</script>
+
+
 import streamlit as st
 from PIL import Image
 from pathlib import Path
@@ -5,19 +17,6 @@ from pathlib import Path
 # STREAMLIT PAGE CONFIG.
 
 st.set_page_config(page_title='GOOGLE ANALYTICS TEST', page_icon='🏢', layout="centered", initial_sidebar_state="collapsed")
-
-google_analytics = """ 
-            <!-- Google tag (gtag.js) -->
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-TSM5N1J350"></script>
-            <script>
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            
-              gtag('config', 'G-TSM5N1J350');
-            </script>
-            """
-st.markdown(google_analytics, unsafe_allow_html=True)
 
 hide_streamlit_style = """ 
             <style>
